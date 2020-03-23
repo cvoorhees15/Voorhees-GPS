5adb70db2d6dd08a1e613df5dcabd672e58fbe3e
Caleb Voorhees <c.voorhees@setonhill.edu>
2020-03-23 09:40:03 -0400

    Initial Commit

---
+++ Voorhees GPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voorhees GPS
+//
+//  Created by Caleb Voorhees on 3/23/20.
+//  Copyright © 2020 Caleb Voorhees. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
